0cffb0693a77f6cf298e72640d79a6d2b86f052a
Mowalky <dev@mowalky.com>
2022-09-27 23:25:06 -0500

    updated fetch

--- src/Fetch.tsx
+++ src/Fetch.tsx
@@ -1,17 +1,37 @@
 import type { Component } from "solid-js";
-import { createResource, For } from "solid-js";
+import { createSignal, createResource, For } from "solid-js";
 
 const Fetch: Component = () => {
+  const [slug, setSlug] = createSignal(
+    "custom-media-players-with-media-chrome"
+  );
+
   const [schedule] = createResource(async () => {
     const getData = await fetch(`https://www.learnwithjason.dev/api/schedule`);
     const data = await getData.json();
     return data;
   });
 
+  const [episode] = createResource(slug, async (slug) => {
+    const getData = await fetch(
+      `https://www.learnwithjason.dev/api/episode/${slug}`
+    );
+    const data = await getData.json();
+    return data;
+  });
+
   return (
-    <div>
-      <For each={schedule()}>{(item: any) => <li>{item.title}</li>}</For>
-    </div>
+    <>
+      <select onChange={(e: any) => setSlug(e.target.value)}>
+        <For each={schedule()}>
+          {(item: any) => (
+            <option value={item.slug.current}>{item.title}</option>
+          )}
+        </For>
+      </select>
+      <hr />
+      {JSON.stringify(episode())}
+    </>
   );
 };
 
